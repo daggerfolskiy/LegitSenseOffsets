a9c72bc134189c46df9a8da59dedf57e969f4a12
daggerfolskiy <142605177+daggerfolskiy@users.noreply.github.com>
2026-07-06 19:34:16 +0300

    updaaaateeeee

--- output/worldrenderer_dll.rs
+++ output/worldrenderer_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-15 12:46:24.048450400 UTC
+// 2026-07-06 16:33:02.092156800 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
