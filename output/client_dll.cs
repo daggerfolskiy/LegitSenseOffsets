b5b860b4b51582432c1f9a270d6540f40eab7153
daggerfolskiy <142605177+daggerfolskiy@users.noreply.github.com>
2026-07-23 01:04:07 +0300

    soundcloud: 3umph - de por vida

--- output/client_dll.cs
+++ output/client_dll.cs
@@ -1,10 +1,10 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-15 20:01:14.368949700 UTC
+// 2026-07-22 22:02:04.766441900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
     // Class count: 563
-    // Enum count: 14
+    // Enum count: 18
     public static class ClientDll {
         // Alignment: 4
         // Member count: 5
--- output/pulse_system_dll.hpp
+++ output/pulse_system_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-15 20:01:14.368949700 UTC
+// 2026-07-22 22:02:04.766441900 UTC
 
 #pragma once
 
